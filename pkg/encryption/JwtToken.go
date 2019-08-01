1591568270d5d9ca58cad9eb70ae4117d6dc8b50
Vardhaman Surana <vds@Vardhamans-MacBook-Air.local>
2019-08-01 12:28:33 +0530

    jwt file added

--- pkg/encryption/JwtToken.go
+++ pkg/encryption/JwtToken.go
@@ -1,1 +1,25 @@
 package encryption
+
+import (
+	"github.com/dgrijalva/jwt-go"
+	"github.com/vds/Restraunt/pkg/models"
+	"time"
+)
+
+func CreateToken(userName string) (string,error){
+	jwtKey:=[]byte("SecretKey")
+	expirationTime:=time.Now().Add(5*time.Minute)
+	claims:=&models.Claims{
+		UserName:userName,
+		StandardClaims: jwt.StandardClaims{
+			ExpiresAt:expirationTime.Unix(),
+		},
+	}
+	//remember to change it later
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	tokenString, err := token.SignedString(jwtKey)
+	if err!=nil{
+		return "",err
+	}
+	return tokenString,nil
+}
